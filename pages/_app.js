22510f780507b09a23d3d7090f1e1dca7a0103a9
Tanel Puström <tanel.pustrom@verkkokauppa.com>
2020-10-31 11:05:05 +0200

    chore: apply new settings/preferences/rules

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import PropTypes from 'prop-types'
+
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
+
+MyApp.propTypes = {
+    Component: PropTypes.node,
+    pageProps: PropTypes.object,
 }
 
 export default MyApp
